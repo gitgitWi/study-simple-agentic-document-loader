d891192d7c0d768cbd5bda54d6b6b05dda833f04
gitgitWi <rs1kflvx@duck.com>
2025-10-21 03:25:44 +0900

    feat(server): download remote file

--- server/documents_route.py
+++ server/documents_route.py
@@ -1,7 +1,8 @@
 from typing import List
 
-from fastapi import APIRouter
-from pydantic import BaseModel, Field
+import httpx
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, Field, HttpUrl
 
 from features.document_loaders import HWPLoader
 
@@ -9,7 +10,7 @@ router = APIRouter()
 
 
 class DocumentLoadRequest(BaseModel):
-    file_url: str = Field(..., description="File path")
+    file_url: HttpUrl | str = Field(..., description="File path")
 
 
 class LoadedDocument(BaseModel):
@@ -23,7 +24,11 @@ class DocumentLoadResponse(BaseModel):
 
 @router.post("/documents", response_model=DocumentLoadResponse)
 async def documents_route(req: DocumentLoadRequest):
-    loader = HWPLoader(req.file_url)
+    local_file_path = await _get_local_file_path(req.file_url)
+    if not local_file_path or len(local_file_path) == 0:
+        raise HTTPException(status_code=400, detail="Invalid file URL")
+
+    loader = HWPLoader(local_file_path)
     documents = await loader.aload()
 
     result = [
@@ -34,3 +39,42 @@ async def documents_route(req: DocumentLoadRequest):
     print("result: ", result)
 
     return DocumentLoadResponse(documents=result)
+
+
+async def _get_local_file_path(file_url: str | HttpUrl) -> str:
+    try:
+        http_url = HttpUrl(file_url)
+        return await _download_file(http_url)
+    except ValueError as _e:
+        return str(file_url)
+
+
+async def _download_file(file_url: HttpUrl) -> str:
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        try:
+            import os
+            from urllib.parse import unquote
+
+            import aiofiles
+
+            print("file_url.path: ", file_url.path)
+            file_name = unquote(os.path.basename(file_url.path or ""))
+            file_path = os.path.join("./downloads", file_name)
+
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+            response = await client.get(str(file_url))
+            response.raise_for_status()
+
+            async with aiofiles.open(file_path, "wb") as file:
+                for chunk in response.iter_bytes(chunk_size=8192):
+                    if chunk:
+                        await file.write(chunk)
+
+            return file_path
+
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(
+                status_code=500,
+                detail=f"Failed to download file: {file_name}",
+            )
