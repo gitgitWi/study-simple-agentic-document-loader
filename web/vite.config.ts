582cd219516ddcbbaef4e5b6beea8237d99e56cf
gitgitWi <gitgitWi@gmail.com>
2025-10-21 15:05:16 +0900

    feat(web): add path alias

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,16 @@
+import path from 'node:path';
 import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, type PluginOption } from 'vite';
 
 export default defineConfig({
   plugins: [preact(), tailwindcss() as PluginOption],
+
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
 
   build: {
     outDir: 'dist',
