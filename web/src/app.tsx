44a1c62bef92650a0d26e67ecc8a60bda8d73ec5
gitgitWi <gitgitWi@gmail.com>
2025-10-22 11:58:48 +0900

    feat(web): add max-width to documents

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -3,9 +3,9 @@ import { Documents } from './features/documents';
 export function App() {
   return (
     <main class="flex flex-col justify-start items-center p-4">
-      <h1 class="font-bold text-3xl">Documents Loader</h1>
+      <h1 class="mb-4 font-bold text-3xl">Documents Loader</h1>
 
-      <Documents.Root>
+      <Documents.Root classNames="max-w-2xl ">
         <Documents.Upload />
         <Documents.Table />
       </Documents.Root>
