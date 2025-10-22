509c7f00edff6728a28d808495cc336e31544a7d
gitgitWi <gitgitWi@gmail.com>
2025-10-22 09:31:51 +0900

    feat(web): change files state to zustand store, use refined types

--- web/src/features/files/index.tsx
+++ web/src/features/files/index.tsx
@@ -1,74 +1,35 @@
-import {
-  createContext,
-  type InputEventHandler,
-  type MouseEventHandler,
-  type SubmitEventHandler,
-} from 'preact';
 import type {
-  Dispatch,
-  PropsWithChildren,
-  SetStateAction,
-} from 'preact/compat';
-import { useCallback, useContext, useRef, useState } from 'preact/hooks';
-import {
-  type DocumentLoaded,
-  fetchLoadDocuments,
-} from '~/features/document-loader';
+  InputEventHandler,
+  MouseEventHandler,
+  SubmitEventHandler,
+} from 'preact';
+import type { PropsWithChildren } from 'preact/compat';
+import { useCallback, useRef } from 'preact/hooks';
+import { fetchLoadDocuments } from '~/features/document-loader';
 import { cn } from '~/utils/class-names';
 import { convertSize } from './convert-size';
+import type { AllowedExtension } from './document.types';
 import { fetchGetSasUrl, fetchUploadFile } from './fetcher';
-import type { AllowedExtension } from './types';
+import { documentsStore } from './use-documents';
 
 export type { AllowedExtension };
 
-export type FileLoaded = {
-  name: string;
-  size: number;
-  sizeDisplay: string;
-  type: AllowedExtension;
-  url: string;
-  documents: DocumentLoaded[];
-};
-
-const FilesContext = createContext<{
-  files: FileLoaded[];
-  setFiles: Dispatch<SetStateAction<FileLoaded[]>>;
-}>({
-  files: [],
-  setFiles: () => {},
-});
-
-const useFilesContext = () => {
-  const context = useContext(FilesContext);
-  if (!context) {
-    throw new Error(
-      'useFilesContext must be used within a FilesContextProvider'
-    );
-  }
-  return context;
-};
-
-function FilesContextProvider({ children }: PropsWithChildren) {
-  // TODO: zustand persist
-  const [files, setFiles] = useState<FileLoaded[]>([]);
-
+function DocumentsRoot({
+  classNames,
+  children,
+}: PropsWithChildren<{ classNames?: string }>) {
   return (
-    <FilesContext.Provider value={{ files, setFiles }}>
-      {children}
-    </FilesContext.Provider>
+    <div class={cn('flex flex-col gap-2 w-full', classNames)}>{children}</div>
   );
 }
 
 function FileUpload() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const { setFiles } = useFilesContext();
+  const { addDocument } = documentsStore.getState();
 
-  const handleSubmit = useCallback<SubmitEventHandler<HTMLFormElement>>(
-    (e) => {
-      e.preventDefault();
-    },
-    [setFiles]
-  );
+  const handleSubmit = useCallback<SubmitEventHandler<HTMLFormElement>>((e) => {
+    e.preventDefault();
+  }, []);
 
   const handleUpload = useCallback<MouseEventHandler<HTMLButtonElement>>(
     (_e) => {
@@ -82,37 +43,46 @@ function FileUpload() {
       const file = e.currentTarget.files?.[0] as File;
       if (!file) return;
 
+      // TODO: Validate file extension
       const fileExtension = file.name.split('.').pop()?.toLowerCase();
       if (!fileExtension) return;
 
-      const { upload_url: uploadUrl = '', download_url: fileUrl = '' } =
-        (await fetchGetSasUrl(fileExtension as AllowedExtension)) ?? {};
-      if (!uploadUrl || !fileUrl) return;
+      const {
+        file_id: fileId,
+        upload_url: uploadUrl = '',
+        download_url: fileUrl = '',
+      } = (await fetchGetSasUrl(fileExtension as AllowedExtension)) ?? {};
+      if (!uploadUrl || !fileUrl || !fileId) {
+        console.warn('Failed to get SAS URL', { fileId, uploadUrl, fileUrl });
+        return;
+      }
 
       const isSuccess = await fetchUploadFile(file, uploadUrl);
       if (!isSuccess) return;
 
-      const { documents = [] } = (await fetchLoadDocuments({
+      const { pages = [] } = (await fetchLoadDocuments({
         fileUrl,
         fileExtension: fileExtension as AllowedExtension,
-      })) ?? { documents: [] };
+      })) ?? { pages: [] };
 
-      setFiles((prev) =>
-        prev.concat({
-          name: file.name,
-          size: file.size,
-          sizeDisplay: convertSize(file.size),
-          type: fileExtension as AllowedExtension,
-          url: fileUrl,
-          documents,
-        } as FileLoaded)
-      );
+      addDocument({
+        id: fileId,
+        name: file.name,
+        size: file.size,
+        sizeDisplay: convertSize(file.size),
+        type: fileExtension as AllowedExtension,
+        url: fileUrl,
+        pages: pages.map(({ page_content, page_number }) => ({
+          number: page_number,
+          content: page_content,
+        })),
+      });
     },
     [inputRef]
   );
 
   return (
-    <form class="flex flex-col gap-2" onSubmit={handleSubmit}>
+    <form class="flex flex-col items-center gap-2" onSubmit={handleSubmit}>
       <input
         ref={inputRef}
         type="file"
@@ -120,7 +90,13 @@ function FileUpload() {
         multiple
         onChange={handleChange}
       />
-      <button type="button" onClick={handleUpload} class={'cursor-pointer'}>
+      <button
+        type="button"
+        onClick={handleUpload}
+        class={
+          'cursor-pointer w-32 px-4 py-2 rounded-md border border-neutral-500 hover:bg-neutral-100 hover:text-neutral-900 transition-all duration-150 ease-out'
+        }
+      >
         Upload
       </button>
     </form>
@@ -128,7 +104,7 @@ function FileUpload() {
 }
 
 function FilesTable() {
-  const { files } = useFilesContext();
+  const { documents } = documentsStore.getState();
 
   return (
     <div class="flex flex-col gap-2 w-full">
@@ -144,11 +120,11 @@ function FilesTable() {
           </tr>
         </thead>
         <tbody>
-          {files.map((file) => (
+          {documents.map((document) => (
             <tr>
-              <td>{file.name}</td>
-              <td>{file.sizeDisplay}</td>
-              <td>{file.type}</td>
+              <td>{document.name}</td>
+              <td>{document.sizeDisplay}</td>
+              <td>{document.type}</td>
               <td>
                 <button type="button">Show Markdown</button>
               </td>
@@ -161,7 +137,7 @@ function FilesTable() {
 }
 
 export const Files = {
-  ContextProvider: FilesContextProvider,
+  Root: DocumentsRoot,
   Upload: FileUpload,
   Table: FilesTable,
 } as const;
