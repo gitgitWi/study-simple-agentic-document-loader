83ed7fbeb6a39fa31dc492ed45b4c0def6b67ce0
gitgitWi <gitgitWi@gmail.com>
2025-10-22 14:50:57 +0900

    feat(loaders): add loader patterns

--- features/document_loaders/get_loader.py
+++ features/document_loaders/get_loader.py
@@ -9,5 +9,25 @@ def get_loader(extension: str, file_path: str) -> BaseLoader | None:
     match extension:
         case AllowedExtension.HWP:
             return HWPLoader(file_path)
+        case AllowedExtension.HWPX:
+            return None
+        case AllowedExtension.PDF:
+            return None
+        case AllowedExtension.DOC:
+            return None
+        case AllowedExtension.DOCX:
+            return None
+        case AllowedExtension.TXT:
+            return None
+        case AllowedExtension.CSV:
+            return None
+        case AllowedExtension.XLS:
+            return None
+        case AllowedExtension.XLSX:
+            return None
+        case AllowedExtension.PPT:
+            return None
+        case AllowedExtension.PPTX:
+            return None
         case _:
             return None
