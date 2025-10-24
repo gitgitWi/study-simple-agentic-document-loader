749c63c35ea9fb575afbb56ad44496e19cf9a3e6
gitgitWi <gitgitWi@gmail.com>
2025-10-24 15:12:01 +0900

    feat(loaders): add parser options argument

--- features/document_loaders/get_loader.py
+++ features/document_loaders/get_loader.py
@@ -1,16 +1,22 @@
 from langchain_core.document_loaders import BaseLoader
 
 from .file_extensions import AllowedExtension
-from .generic import get_generic_loader
+from .generic import ParserOptions, get_generic_loader
 from .hwp import HWPLoader
 
 
 # TODO: file_path 받지 말고 loader 클래스만 반환
-def get_loader(extension: str, file_path: str) -> BaseLoader | None:
+def get_loader(
+    extension: str, file_path: str, parser_options: ParserOptions
+) -> BaseLoader | None:
     match extension:
         case AllowedExtension.HWP:
             return HWPLoader(file_path)
         case AllowedExtension.HWPX:
             return None
         case _:
-            return get_generic_loader(file_path, extension)
+            return get_generic_loader(
+                file_path,
+                extension,
+                parser_options,
+            )
